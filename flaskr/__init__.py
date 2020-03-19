9f6bf07c0701dca54fd73b9b5dde24994ac3ce60
shehan shaman <33275058+shehanshaman@users.noreply.github.com>
2020-03-20 00:29:50 +0530

    Visualization added to the app
    Signed-off-by: shehan shaman <33275058+shehanshaman@users.noreply.github.com>

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -41,11 +41,12 @@ def create_app(test_config=None):
     db.init_app(app)
 
     # apply the blueprints to the app
-    from flaskr import auth, blog, preprocess
+    from flaskr import auth, blog, preprocess, visualization
 
     app.register_blueprint(auth.bp)
     app.register_blueprint(blog.bp)
     app.register_blueprint(preprocess.bp)
+    app.register_blueprint(visualization.bp)
 
     # make url_for('index') == url_for('blog.index')
     # in another app, you might define a separate main index here with
