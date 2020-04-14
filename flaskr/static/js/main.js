891a72ae3630d8eb9f43e2e63b8a0dcd19182f93
shehan shaman <33275058+shehanshaman@users.noreply.github.com>
2020-04-14 19:27:01 +0530

    Google sigin script adding for google users only change file for home

--- flaskr/static/js/main.js
+++ flaskr/static/js/main.js
@@ -3,16 +3,7 @@ $(document).ready(function () {
     $('.sidenav').sidenav();
 });
 
-function onLoad() {
-    gapi.load('auth2', function () {
-        gapi.auth2.init();
-    });
-}
-
 //Google signOut
 function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        window.location.replace('/auth/logout');
-    });
+    window.location.replace('/auth/logout');
 }
